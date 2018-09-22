7e93b87c3f02b1ac20adfe055236b0dfaf75bf59
laboratorio <laboratorio@SP-LAB9-13>
2018-09-22 15:46:26 -0600

    g

---
+++ ejercicio 1 semana2/ejercicio 1 semana2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ejercicio_1_semana2.MvcApplication" Language="C#" %>
---
+++ ejercicio 1 semana2/ejercicio 1 semana2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ejercicio_1_semana2.Startup))]
+namespace ejercicio_1_semana2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
